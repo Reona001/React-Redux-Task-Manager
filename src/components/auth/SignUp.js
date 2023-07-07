5f3200d22b997dbeece54ecf527c92a5ed1170cb
reona kasuya <reon.kasuya@gmail.com>
2023-07-07 18:54:22 +0900

    Project create form created

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -14,6 +14,7 @@ class SignUp extends Component {
   handleChange = (event) => {
     console.log(event);
     this.setState({
+      // Simplifies the data handling by using id
       [event.target.id]: event.target.value,
     });
   };
