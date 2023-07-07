5e55efbdf4ace444bd8976b0f8167506cd70e318
reona kasuya <reon.kasuya@gmail.com>
2023-07-07 15:28:48 +0900

    Picking up url id

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-const ProjectDetails = () => {
+const ProjectDetails = (props) => {
+  console.log(props);
+  // You will see an object that has properties such as history and url ids.
+
+  const id = props.match.params.id;
+
   return (
     <div className="container sectio project-details">
       <div className="card z-depth-0">
         <div className="card-content">
-          <span className="card-title">Project Title</span>
+          <span className="card-title">Project Title {id}</span>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
             culpa possimus alias nesciunt, architecto laborum nemo explicabo
