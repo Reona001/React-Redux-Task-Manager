3a981d5eedbe1b702737ffb151e451a82a45d558
reona kasuya <reon.kasuya@gmail.com>
2023-07-09 00:03:59 +0900

    Project summary in project list is now outputting data from redux

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -2,14 +2,18 @@ import React from "react";
 
 import ProjectSummary from "./ProjectSummary";
 
-const ProjectList = () => {
+// && means if there is projects do this
+const ProjectList = ({ projects }) => {
   return (
     <div className="project-list section">
+      {projects &&
+        projects.map((project) => {
+          return <ProjectSummary project={project} key={project.id} />;
+        })}
+      {/* <ProjectSummary />
       <ProjectSummary />
       <ProjectSummary />
-      <ProjectSummary />
-      <ProjectSummary />
-      <ProjectSummary />
+      <ProjectSummary /> */}
     </div>
   );
 };
