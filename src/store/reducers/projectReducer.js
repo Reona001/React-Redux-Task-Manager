d4b63317014cafcd6b974a838a4a15bad7722f6f
reona kasuya <reon.kasuya@gmail.com>
2023-07-08 23:43:41 +0900

    Created dummy data for projects

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,24 @@
-const initState = {};
+const initState = {
+  projects: [
+    { id: "1", title: "help find peach", content: "go down the green pipes" },
+    {
+      id: "2",
+      title: "collect the stars",
+      content: "fly to the skies and grab the stars",
+    },
+    {
+      id: "3",
+      title: "Make yoshi your friend",
+      content: "abduct a yoshi baby",
+    },
+    {
+      id: "4",
+      title: "Install a new internet Bowser",
+      content: "download Koopa bowser",
+    },
+    { id: "5", title: "Burn the world down", content: "fire flower" },
+  ],
+};
 
 const projectReducer = (state = initState, action) => {
   return state;
