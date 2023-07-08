c165c8929a14248bff3b730579f90919b3b86d4b
reona kasuya <reon.kasuya@gmail.com>
2023-07-08 23:39:29 +0900

    Store defined for provider

--- src/index.js
+++ src/index.js
@@ -6,14 +6,17 @@ import reportWebVitals from "./reportWebVitals";
 
 import { createStore } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
+import { Provider } from "react-redux";
 
 const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
