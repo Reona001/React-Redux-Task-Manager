23b783747a542c05f474eeb933dd4f97be852533
reona kasuya <reon.kasuya@gmail.com>
2023-07-10 23:50:31 +0900

    Firebase added using npm and config file made

---
+++ src/config/fbConfig.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// The api config info here is mostly for the app to identify, and know which firebase it needs to connect to
+// so it doesn't need to be available to the user side/
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCUs2eFgbosfEIC5hqN85Y091t12SfDOgY",
+  authDomain: "mario-plan-c9c7c.firebaseapp.com",
+  projectId: "mario-plan-c9c7c",
+  storageBucket: "mario-plan-c9c7c.appspot.com",
+  messagingSenderId: "894696476570",
+  appId: "1:894696476570:web:03bfe5f5394a4db92eb423",
+  measurementId: "G-X7YXDHVSYK",
+};
