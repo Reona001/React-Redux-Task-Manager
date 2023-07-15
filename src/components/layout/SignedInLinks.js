c49204e2c907b639dfcccf6a360e6e91376c5bb0
reona kasuya <reon.kasuya@gmail.com>
2023-07-15 23:05:21 +0900

    Logout feature added and functioning

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import { signOut } from "../../store/actions/authActions";
 // We can use a functional component not a class component because we won't be using a state
 // We want to use conditionals to show logout and signin links
 
-const SignedInLinks = () => {
+const SignedInLinks = (props) => {
   return (
     <ul className="right">
       <li>
         <NavLink to="/create">New Project</NavLink>
       </li>
       <li>
-        <NavLink to="/">Log Out</NavLink>
+        <a onClick={props.signOut}>Logout</a>
       </li>
       <li>
         <NavLink to="/" className="btn btn-floating pink lighten-1">
@@ -24,4 +26,11 @@ const SignedInLinks = () => {
   );
 };
 
-export default SignedInLinks;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signOut: () => dispatch(signOut()),
+  };
+};
+
+// export default SignedInLinks;
+export default connect(null, mapDispatchToProps)(SignedInLinks);
--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -14,6 +14,9 @@ const authReducer = (state = initState, action) => {
         ...state,
         authError: null,
       };
+    case "SIGNOUT_SUCCESS":
+      console.log("Signout Successful");
+      return state;
     default:
       return state;
   }
