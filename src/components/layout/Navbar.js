4169aa7efd0bd1f10778ca019ce876027aef9440
reona kasuya <reon.kasuya@gmail.com>
2023-07-16 23:48:52 +0900

    Using authetication the authentication status from the state.firebase.auth to return different content depending on the status

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -7,15 +7,19 @@ import { connect } from "react-redux";
 // We can use a functional component not a class component because we won't be using a state
 // We want to use conditionals to show logout and signin links
 
-const Navbar = () => {
+const Navbar = (props) => {
+  const { auth } = props;
+  console.log(auth);
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
+  //if uid exists is true and render signed in links
+
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">
           MarioPlan
         </Link>
-        <SignedInLinks />
-        <SignedOutLinks />
+        {links}
       </div>
     </nav>
   );
@@ -23,7 +27,9 @@ const Navbar = () => {
 
 const mapStateToProps = (state) => {
   console.log(state);
-  return {};
+  return {
+    auth: state.firebase.auth,
+  };
 };
 // export default Navbar;
 export default connect(mapStateToProps)(Navbar);
